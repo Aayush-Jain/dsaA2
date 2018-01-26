5ab8590c2873d75ba7a999205435ae37591ce497
Aayush Jain <jain98aayush@gmail.com>
2018-01-26 22:39:29 +0530

    one question

--- dsa18.cpp
+++ dsa18.cpp
@@ -5,49 +5,63 @@ using namespace std;
 
 struct dict{
 	char c;
-	char key[10];
+	char key[100];
 };
-
+int findchar(char a,dict map[100]){
+	for(int i=1;i<=37;i++){
+		if(a==map[i].c){
+			return i;
+			break;
+		}
+	}
+}
 int main(){
+	char str[1000];
+	scanf("%[^\n]%*c",str);
+	int l = strlen(str);
 	struct dict map[100];
-	map[1].c = 'a'; map[1].key[] = {'2','3','4','\0'};
-	/*map[2].c = 'b'; map[2].key = ;
-	map[3].c = 'c'; map[3].key = ;
-	map[4].c = 'd'; map[4].key = ;
-	map[5].c = 'e'; map[5].key = ;
-	map[6].c = 'f'; map[6].key = ;
-	map[7].c = 'g'; map[7].key = ;
-	map[8].c = 'h'; map[8].key = ;
-	map[9].c = 'i'; map[9].key = ;
-	map[10].c = 'j'; map[10].key = ;
-	map[11].c = 'k'; map[11].key = ;
-	map[12].c = 'l'; map[12].key = ;
-	map[13].c = 'm'; map[13].key = ;
-	map[14].c = 'n'; map[14].key = ;
-	map[15].c = 'o'; map[15].key = ;
-	map[16].c = 'p'; map[16].key = ;
-	map[17].c = 'q'; map[17].key = ;
-	map[18].c = 'r'; map[18].key = ;
-	map[19].c = 's'; map[19].key = ;
-	map[20].c = 't'; map[20].key = ;
-	map[21].c = 'u'; map[21].key = ;
-	map[22].c = 'v'; map[22].key = ;
-	map[23].c = 'w'; map[23].key = ;
-	map[24].c = 'x'; map[24].key = ;
-	map[25].c = 'y'; map[25].key = ;
-	map[26].c = 'z'; map[26].key = ;
-	map[27].c = '0'; map[27].key = ;
-	map[28].c = '1'; map[28].key = ;
-	map[29].c = '2'; map[29].key = ;
-	map[30].c = '3'; map[30].key = ;
-	map[31].c = '4'; map[31].key = ;
-	map[32].c = '5'; map[32].key = ;
-	map[33].c = '6'; map[33].key = ;
-	map[34].c = '7'; map[34].key = ;
-	map[35].c = '8'; map[35].key = ;
-	map[36].c = '9'; map[36].key = ;*/
-	/*map[37].c = ''; map[37].key = ;
-	map[38].c = ''; map[38].key = ;*/
-	cout << map[1].key;	
+	
+	map[1].c = 'a'; strcpy(map[1].key,"2");
+	map[2].c = 'b'; strcpy(map[2].key,"22");
+	map[3].c = 'c'; strcpy(map[3].key,"222");
+	map[4].c = 'd'; strcpy(map[4].key,"3");
+	map[5].c = 'e'; strcpy(map[5].key,"33");
+	map[6].c = 'f'; strcpy(map[6].key,"333");
+	map[7].c = 'g'; strcpy(map[7].key,"4");
+	map[8].c = 'h'; strcpy(map[8].key,"44");
+	map[9].c = 'i'; strcpy(map[9].key,"444");
+	map[10].c = 'j'; strcpy(map[10].key,"5");
+	map[11].c = 'k'; strcpy(map[11].key,"55");
+	map[12].c = 'l'; strcpy(map[12].key,"555");
+	map[13].c = 'm'; strcpy(map[13].key,"6");
+	map[14].c = 'n'; strcpy(map[14].key,"66");
+	map[15].c = 'o'; strcpy(map[15].key,"666");
+	map[16].c = 'p'; strcpy(map[16].key,"7");
+	map[17].c = 'q'; strcpy(map[17].key,"77");
+	map[18].c = 'r'; strcpy(map[18].key,"777");
+	map[19].c = 's'; strcpy(map[19].key,"7777");
+	map[20].c = 't'; strcpy(map[20].key,"8");
+	map[21].c = 'u'; strcpy(map[21].key,"88");
+	map[22].c = 'v'; strcpy(map[22].key,"888");
+	map[23].c = 'w'; strcpy(map[23].key,"9");
+	map[24].c = 'x'; strcpy(map[24].key,"99");
+	map[25].c = 'y'; strcpy(map[25].key,"999");
+	map[26].c = 'z'; strcpy(map[26].key,"9999");
+	map[27].c = '0'; strcpy(map[27].key,"00");
+	map[28].c = '1'; strcpy(map[28].key,"11");
+	map[29].c = '2'; strcpy(map[29].key,"2222");
+	map[30].c = '3'; strcpy(map[30].key,"3333");
+	map[31].c = '4'; strcpy(map[31].key,"4444");
+	map[32].c = '5'; strcpy(map[32].key,"5555");
+	map[33].c = '6'; strcpy(map[33].key,"6666");
+	map[34].c = '7'; strcpy(map[34].key,"77777");
+	map[35].c = '8'; strcpy(map[35].key,"8888");
+	map[36].c = '9'; strcpy(map[36].key,"99999");
+	map[37].c = ' '; strcpy(map[37].key,"0");
+	//map[38].c = ''; map[38].key = ;*/
+	for(int i=0;i<l;i++){
+		int a = findchar(str[i],map);
+		cout << map[a].key;
+	}
 	return 0;
 }
